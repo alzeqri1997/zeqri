d8fbfcde4e2597a2a07a7155f9b1a46326709ffe
alzeqri1997 <ahmed1221moh@gmail.com>
2022-12-13 21:08:31 +0300

    create contact me

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,42 @@
-import Head from 'next/head'
-import Divider from '../components/Divider'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import { Pattern } from '../components/icons'
-import Projects from '../components/Projects'
-import WhatIDo from '../components/WhatIDo'
+/** @format */
+
+import Head from "next/head"
+import ContactMe from "../components/ContactMe"
+import Divider from "../components/Divider"
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+import { Pattern } from "../components/icons"
+import Projects from "../components/Projects"
+import WhatIDo from "../components/WhatIDo"
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Zeqri</title>
-        <meta name="description" content="zeqri for web development and design" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div>
+			<Head>
+				<title>Zeqri</title>
+				<meta
+					name="description"
+					content="zeqri for web development and design"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main >
-        <div className='relative light-bg' >
-          <Header />
-          <Hero/>
-        </div>
-        <Divider text={'What I do exactly'} />
-        <WhatIDo/>
-        <Divider text={"Some of Things I’ve Built"} />
-        <div className='light-bg relative hidden' >
-          <Pattern classes={'top-pattern'}/>
-          <Projects />
-          <Pattern classes={'bottom-pattern'}/>
-        </div>
-      </main>
-    </div>
-  )
+			<main>
+				<div className="relative light-bg">
+					<Header />
+					<Hero />
+				</div>
+				<Divider text={"What I do exactly"} />
+				<WhatIDo />
+				<Divider text={"Some of Things I’ve Built"} />
+				<div className="light-bg relative hidden">
+					<Pattern classes={"top-pattern"} />
+					<Projects />
+					<Pattern classes={"bottom-pattern"} />
+				</div>
+				<Divider text={"Contact Me"} />
+				<ContactMe />
+			</main>
+		</div>
+	)
 }
