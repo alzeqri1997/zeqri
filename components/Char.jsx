1100a74d47255679ffaa27d40004b383e2ae2654
alzeqri1997 <ahmed1221moh@gmail.com>
2022-12-16 23:32:29 +0300

    create Char component

---
+++ components/Char.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Char = ({children, charClass = ''}) => {
+  const chars = children.split('');
+  console.log('chars',chars)
+  return (
+    chars.map((char, index) => {
+      if (char === ' ') return ' ';
+      return<div key={index} className={charClass} >{char}</div>
+    })
+  )
+}
+
+export default Char
