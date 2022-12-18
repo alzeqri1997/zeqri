2f53c4da558f9d85f212f9af9c292695e4b2df46
alzeqri1997 <ahmed1221moh@gmail.com>
2022-12-18 10:01:14 +0300

    animate AboutMe section

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -1,24 +1,86 @@
 import Link from 'next/link'
-import React from 'react'
+import React, {useRef, useLayoutEffect} from 'react'
 import myPic from '../assets/images/myPic.png'
 import Divider from './Divider'
-import {  LinkedIn, Pattern2, Twitter} from './icons'
+import { LinkedIn, Pattern2, Twitter } from './icons'
+import {gsap} from 'gsap'
+import ScrollTrigger from 'gsap/dist/ScrollTrigger'
+import Char from './Char'
 
 
 const AboutMe = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  const aboutMe = useRef();
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.timeline({
+        defaults: {
+          duration: 0.5,
+        },
+        scrollTrigger: {
+          trigger: '.about-me',
+          start: 'top center',
+        }
+      }).fromTo('.about-me__image', {
+        opacity: 0,
+        x: 100,
+      }, {
+        opacity: 1,
+        x:0
+      }, "<").to('.about-me__image--overlay', {
+        duration: 0.65,
+        delay: 0.1,
+        width: 0,
+        ease: "power2.out"
+      }).fromTo('.about-me__image-img',{
+        scale: 1.2,
+      }, {
+        scale: 1,
+        delay:0.1,
+      }, "<").fromTo('.about-me__content', {
+        opacity: 0,
+        x: -100,
+      }, {
+        opacity: 1,
+        x: 0
+      }, "-=.5")
+        .to('.about-me__content--heading-char', {
+        y: 0,
+        stagger: 0.03,
+        delay: 0.2,
+        duration: .4,
+      }, "-=.3").fromTo('.about-me__content--text', {
+        opacity: 0,
+        x: -100,
+      }, {
+        opacity: 1,
+        delay: 0.3,
+        stagger: 0.1,
+        x: 0
+      }, "-=.5").fromTo('.about-me__content--icons',{
+        opacity: 0,
+        x: -100,
+      }, {
+        opacity: 1,
+        x: 0
+      }, "-=.5")
+    }, aboutMe)
+    return () => ctx.revert()
+  },[])
   return (
-    <section id='about' className='light-bg relative '>
+    <section ref={aboutMe} id='about' className='light-bg relative '>
 				<div className='container' >
           <Divider text={"About Me"} />
         </div>
       <div className='relative hidden' >
         <div className='container about-me' >
           <div className='about-me__content'>
-            <h1 className='about-me__content--heading'>A Few Words About me</h1>
-            <p>I’m <strong>Ahmed Al-zeqri</strong>, a freelance young and passionate <strong>developer</strong> & <strong>designer</strong> <br />
+            <h1 className='about-me__content--heading'><Char charClass='about-me__content--heading-char' >A Few Words About me</Char></h1>
+            <p className='about-me__content--text'>I’m <strong>Ahmed Al-zeqri</strong>, a freelance young and passionate <strong>developer</strong> & <strong>designer</strong> <br />
               who loves his work.</p>
             <br />
-            <p>As a web designer and developer, I believe good design is about finding the <br />
+            <p className='about-me__content--text'>As a web designer and developer, I believe good design is about finding the <br />
               right balance between aesthetic design and functionality. I aim to create <br />
               beautiful, user-friendly designs to help my clients achieve their business goals. <br />
               If you're looking for or <strong>Web Designer</strong> or <strong>Front End Developer</strong> who can help <br />
@@ -29,7 +91,9 @@ const AboutMe = () => {
               <Link href={"#"}><Twitter/></Link>
               </div>
           </div>
-          <img className='about-me__img' src={myPic.src} alt="about me ahmed alzagri" />
+          <div className='about-me__image'>
+            <div className='about-me__image--overlay'></div>
+            <img className='about-me__image-img'  src={myPic.src} alt="about me ahmed alzagri" /></div>
         </div>
       <Pattern2 classes={'right-pattern'} />
       <Pattern2 classes={'left-pattern'} />
