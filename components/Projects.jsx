bf4152f813b6befae7915f96928ec56f5f989ad6
alzeqri1997 <ahmed1221moh@gmail.com>
2022-12-19 01:19:27 +0300

    update project animation

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,31 +3,40 @@ import Divider from './Divider'
 import Project from './Project'
 import ScrollTrigger from 'gsap/dist/ScrollTrigger'
 import {gsap} from 'gsap'
+import { Pattern } from './icons'
 
 const Projects = () => {
   gsap.registerPlugin(ScrollTrigger)
-  const tl = useRef();
   const projects = useRef();
   
-  // useLayoutEffect(() => {
-  //   const ctx = gsap.context(() => {
-  //     tl.current = gsap
-  //       .timeline({ defaults: { duration: 0.5 }, scrollTrigger: {
-  //         trigger: '.project',
-  //         start: 'top center',
-  //         markers: true
-  //       }
-  //       }).from('.project', {
-  //         opacity:0,
-  //       })
-  //   }, projects)
-  //   return () => ctx.revert();
-  // }, [])
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.to('.top-pattern-parallax', {
+        scrollTrigger: {
+          trigger: '.project',
+            start: '-=30% center',
+            markers: true
+        },
+        keyframes: {
+          opacity: [0, 1,0]
+        },
+        repeat: -1,
+        duration: 2,
+        repeatDelay: 0.5
+      })
+    }, projects)
+    return () => ctx.revert();
+  }, [])
   return (
-    <section ref={projects} id='work' className='projects container relative' >
-      <Divider text={"Some of Things I’ve Built"} />
-      <Project />
-    </section>
+    <div ref={projects} className='light-bg relative' >
+      <Pattern classes='top-pattern'/>
+      <Pattern classes='top-pattern top-pattern-parallax'/>
+      <section id='work' className='projects container relative' >
+        <Divider text={"Some of Things I’ve Built"} />
+        <Project />
+      </section>
+      <Pattern classes='bottom-pattern'/>
+    </div>
   )
 }
 
