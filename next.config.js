54c39e1733007b07295d8d059baaf1c4494af6c6
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2025-07-11 15:24:02 +0300

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  basePath:"/zeqri/"
+  // basePath:"/zeqri/"
 }
 
 module.exports = nextConfig
